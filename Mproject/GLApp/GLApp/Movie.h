ea44026313c96fcafa3fd4f41327bf174d2f7aeb
Peyman Jahangirlou <pjahangi@ucsd.edu>
2021-05-06 15:15:20 -0700

    Update Movie.h

--- Mproject/GLApp/GLApp/Movie.h
+++ Mproject/GLApp/GLApp/Movie.h
@@ -19,24 +19,33 @@ public:
 		  const std::string& language,
 		  const std::string& sourceEntity,
 		  const std::string& imgUrl);
+
+	/* @brief copy constructor */
+	Movie(const Movie& other);
 		
 	/* inline getters */
 	inline const std::string& title() const { return M_title; }
 	inline const std::string& language() const { return M_language; }
 	inline const std::string& sourceEntity() const { return M_sourceEntity; }
 	inline const std::string& imgUrl() const { return M_imgUrl; }
+	inline bool isLastItemOnViewPort() const { return M_isLastItemOnViewPort; }
+	inline bool isFirstItemOnViewPort() const { return M_isFirstItemOnViewPort;; }
 
 	/* inline setters */
 	inline void settitle(const std::string& title) { M_title = title; }
 	inline void setLanguage(const std::string& language) { M_language = language; }
 	inline void setSourceEntity(const std::string& sourceEntity) { M_sourceEntity = sourceEntity; }
 	inline void setImgUrl(const std::string& imgUrl) { M_imgUrl = imgUrl; }
-
+	inline void setIsLastItemOnViewPort(bool on) { M_isLastItemOnViewPort = on; }
+	inline void setIsFirstItemOnViewPort(bool on) { M_isFirstItemOnViewPort = on; }
 private:
 	std::string		M_title;
 	std::string		M_language;
 	std::string		M_sourceEntity;
 	std::string		M_imgUrl;
+	bool	M_isLastItemOnViewPort;
+	bool	M_isFirstItemOnViewPort;
+
 
 };
 
